9919ed039732b9163e9ebebe2bb5e740ae370c46
deboracasa <pekkyalves01@gmail.com>
2022-09-10 22:16:42 -0300

    vscode casa !

---
+++ java.js
@@ -0,0 +1,13 @@
+ //alert( "exercicios de logica ")
+
+ //alert ("10 + 7") 
+ //alert (" 9 * 8 ") 
+ // alert ("60 % 3")
+
+  // var num1 = prompt  ("digite o primeiro numero ");
+   //var num2 = prompt ("digite o segundo numero" );
+   //alert (9 * num1);
+   //alert (num1 / num2);
+
+ //var nome = prompt('Qual é o seu nome?');
+//alert('Olá ' + nome + ', é um prazer te ver!');
