747a83eb4e232e2199dcb77e25c63397b008e20a
deboracasa <pekkyalves01@gmail.com>
2022-09-11 21:25:41 -0300

    you tuber

---
+++ exercicios de teste/exercicio 1.js
@@ -0,0 +1,11 @@
+//alert ("exercicio de java ")
+
+
+
+//var nome = prompt ("diga seu nome ")
+// var ano = prompt (" escreva o ano que vc nasceu")
+// var idade = 2022 - ano 
+// var dia= 365*idade
+// var mes = dia/30
+// alert (`seu nome: ${nome} , sua idade é ${idade} ,vc vai morrer dia ${dia} , no mes${mes} `)
+// //alert (nome+"\n"+ " voce tem: " +idade +"anos"  +"\n"+"voce tem: "+dia+ "dias"+ "\n"+"voce tem: " + mes +"mes" )
---
+++ yt.js
@@ -0,0 +1,100 @@
+// let suaIdade =5 ;
+// console.log ("sua idade é: " +suaIdade);
+// let idadeDoIrmao = 30;
+// console.log ("seu irmao tem: " +idadeDoIrmao  );
+// ===================================================//
+// camelCase <errado> 
+//  let Nome ='debora';
+//  console.log (nome);
+// camelCase <correto> 
+//  let nome ='debora';
+//  console.log (nome);
+// atencao nas diferencas entre letras maiuscula e minuscula 
+// ==================================================//
+// CONSTANTES
+// let valorIngressoAdulto=20; 
+// valorIngressoAdulto =30;
+// console.log (valorIngressoAdulto);
+// let deixa eu redefini o valor da varievel quando eu quise
+// const 
+// usa const quando tem valores que nao precisam se redefinido
+// =======================================================//
+// TIPOS PRIMITIVOS 
+//  number ( idade 16 ou peso 17.9 ou altura)
+//  string ( nome, comida ,filme favorito)
+//  true = boolean (algo esta aprovado ou nao  )
+//  undefined
+//  null 
+//  =========================================================
+// TIPAGEM DINAMICA
+// let nome = 'Mat'; 
+// diz qual o tipo primitivo da vareavel
+// =============================================================
+// OBJETOS
+// tem como objetivo junta tudo em um lugar so , podendo acessa pela 
+// propiedade do objeto
+// let pessoa ={
+//      nome: 'marta'
+// };
+
+// console.log(pessoa);
+
+// ==============================================================
+//  ARRAYS
+//  conjuto de dados
+// let familia = [26 , 45,50,17];
+// console.log (familia [2]);
+// =================================================================
+// FUNCTION
+// verbo+acao desse verbo
+// function resetaCor(){
+//     corSite =" ";
+
+// }
+// =================================================================
+// operadore aritimeticos 
+//   let idade = 19
+//   console.log(idade++);
+//   console.log (idade); 
+// ou 
+// console.log(++idade)
+// ===============================
+// operadore de atibuicao 
+// let valordabanana = 5;
+// valordabanana+=valordabanana;
+// console.log (valordabanana);
+// =======================================
+// operadore de igualdade 
+// retorna true ou false 
+// console.log (1===3);
+// ==========================================
+// operador ternario
+// um cliente tem 100 pontos diga qual a classe do cliente , premiun ou comum
+//  let ponto = 100;
+// let tipo = ponto >100? 'premium' : 'comun' ;
+// console.log(tipo); 
+// ==================================================
+// operdore logicos
+// e (&&)
+// retorna true se os dois operadore forem verdadeiro (os mesmo)
+// let maiordeidade = true;
+// let possuicarteiradetrabalho = true
+// let podeaplicar=maiordeidade && possuicarteiradetrabalho;
+// console.log(podeaplicar);
+// ======================================
+// operador logico 
+// ou (||)
+// retorna  true se um dos operador forem verdadeiro
+//  let maiordeidade = false;
+//  let possuicarteiradetrabalho = true
+//  let podeaplicar=maiordeidade || possuicarteiradetrabalho;
+// // console.log(podeaplicar);
+// ======================================
+// //  operador Not (!)
+//  let candidatorecusado =!podeaplicar
+
+// console.log(candidatorecusado)
+
+
+
+
